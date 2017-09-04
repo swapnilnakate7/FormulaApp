d83cd84a82d02ae0c30ed5385bae2e60bbfa336f
nakate.swapnil7@gmail.com <nswapnil7>
2017-09-05 01:08:27 +0530

    Minor Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormulaListComponent } from './formula-list/formula-list.component';
+import { FormulaComponent } from './formula/formula.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormulaListComponent,
+    FormulaComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/formula/formula.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formula',
+  templateUrl: './formula.component.html',
+  styleUrls: ['./formula.component.css']
+})
+export class FormulaComponent implements OnInit {
+title="`F=ma`";
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
